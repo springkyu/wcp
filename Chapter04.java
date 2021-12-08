f0f15ae4108672c9aeab62552fdafac55a914040
springkyu <86840763+springkyu@users.noreply.github.com>
2021-12-08 23:11:09 +0900

    [UPDATE]確認問題

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        final int LUCKY_NUMBER = 7;
+        String str = "ウェブ　キャンプ"
+        System.out.println(str);
+        str = "エンジニア";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
