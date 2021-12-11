de330a84cf2cc1fd1f3651e273dcc99b5c15c9f9
springkyu <86840763+springkyu@users.noreply.github.com>
2021-12-11 23:36:59 +0900

    [UPDATE]確認問題

--- Chapter05.java
+++ Chapter05.java
@@ -1,5 +1,6 @@
 public class Chapter05 {
     public static void main(String[] args) {
+        // プリミティブ型の「long」「double」「boolean」の変数
         long l = 10000000000L;
         System.out.println(l);
         double d = 3.14;
