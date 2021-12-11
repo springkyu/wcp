5f40749ab1f720444fea69e4b343b486cc47a25c
springkyu <86840763+springkyu@users.noreply.github.com>
2021-12-11 23:36:16 +0900

    [UPDATE]確認問題

--- Chapter05.java
+++ Chapter05.java
@@ -2,5 +2,8 @@ public class Chapter05 {
     public static void main(String[] args) {
         long l = 10000000000L;
         System.out.println(l);
+        double d = 3.14;
+        System.out.println(d);
+
     }
 }
