bf632818996b40dabed245f20c88db36ae9d41c5
springkyu <86840763+springkyu@users.noreply.github.com>
2021-12-12 22:12:20 +0900

    [UPFDATE]確認問題/インクリメント

--- Chapter06.java
+++ Chapter06.java
@@ -2,5 +2,8 @@ public class Chapter06 {
     public static void main(String[] args) {
         int val = 2 + 5;
         System.out.println(val);
+
+        val++;
+        System.out.println(val);
     }
 }
