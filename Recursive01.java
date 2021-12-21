3d20f071e85fcb3a43b717a4dafbb661f3f37e70
springkyu <86840763+springkyu@users.noreply.github.com>
2021-12-21 18:30:25 +0900

    [UPDATE]サンプルプログラム

--- Recursive01.java
+++ Recursive01.java
@@ -3,4 +3,13 @@ public class Recursive01 {
     int number = 1;
     printNum(number);
   }
+
+  public static int printNum(int number) {
+    if (number < 50) {
+      number *= 2;
+      System.out.println("While01 = " + number);
+      printNum(number);
+    }
+    return number;
+  }
 }
