cdd80e127236b1bccb62db9218117510935b7271
springkyu <86840763+springkyu@users.noreply.github.com>
2021-12-22 22:45:50 +0900

    [UPDATE]確認問題

--- Chapter08.java
+++ Chapter08.java
@@ -7,6 +7,7 @@ public class Chapter08 {
             num++;
         }
 
+        // 2. 通常「for」文
         int array[] = { 1, 4, 14, 25 };
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
