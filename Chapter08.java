4b30a64980f8ea44884dc84cb3cf6228004a2c77
springkyu <86840763+springkyu@users.noreply.github.com>
2021-12-22 22:44:44 +0900

    [UPDATE]確認問題

---
+++ Chapter08.java
@@ -0,0 +1,9 @@
+public class Chapter08 {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num < 5) {
+            System.out.println(num * num);
+            num++;
+        }
+    }
+}
